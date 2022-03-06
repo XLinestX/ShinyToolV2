f39f1dd3849adaf135f2bb55015993202f5cdd7b
XLinestX <74462856+XLinestX@users.noreply.github.com>
2022-03-06 23:40:10 +0100

    Create games.lua

---
+++ games.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 8540346411 then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyToolV2/main/RBX.lua"))()
+  end
+if game.PlaceId == 8737602449 then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyToolV2/main/PLSD.lua"))()
+        end
