a4fdb70ab3e412d06927f1f1c1201af47263e599
XLinestX <74462856+XLinestX@users.noreply.github.com>
2022-03-12 15:28:51 +0100

    Update games.lua

--- games.lua
+++ games.lua
@@ -10456,3 +10456,6 @@ if game.PlaceId == 8540346411 then
 if game.PlaceId == 8737602449 then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyToolV2/main/PLSD.lua"))()
         end
+if game.PlaceId == 7560156054 then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyToolV2/main/CLS.lua"))()
+        end
