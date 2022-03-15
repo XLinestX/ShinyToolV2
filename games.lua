15b78ea2b24896f628dbb7d307a68e87333d60bf
XLinestX <74462856+XLinestX@users.noreply.github.com>
2022-03-15 23:02:48 +0100

    Update games.lua

--- games.lua
+++ games.lua
@@ -10451,11 +10451,126 @@
 
 
 if game.PlaceId == 8540346411 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyToolV2/main/RBX.lua"))()
-  end
+    --Rebirth Champions X
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyToolV2/main/RBX.lua"))()
+end
+-------------------------------------
 if game.PlaceId == 8737602449 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyToolV2/main/PLSD.lua"))()
-        end
+    --PLS DONATE
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyToolV2/main/PLSD.lua"))()
+end
+-------------------------------------
 if game.PlaceId == 7560156054 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyToolV2/main/CLS.lua"))()
-        end
+    --Clicker Simulator
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyToolV2/main/CLS.lua"))()
+end
+-------------------------------------
+if game.PlaceId == 6284583030 then
+    --Pet Simulator X
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyToolSupported/main/ShinyTool.lua"))()
+end
+-------------------------------------
+if game.PlaceId == 8592863835 then
+    --Magnet Simulator 2
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyToolSupported/main/MagnetSim2"))()
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--Nop
