591bc3719188d1a0ea929d62e84f8db25bafca62
XLinestX <74462856+XLinestX@users.noreply.github.com>
2022-04-02 20:19:55 +0200

    Create premium_loader.lua

---
+++ premium_loader.lua
@@ -0,0 +1,7 @@
+if syn then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyToolSupported/main/ShinyToolPremium"))()
+else
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/Shiny-Tool/main/iris.lua"))()
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyToolSupported/main/ShinyToolPremium"))()
+end
